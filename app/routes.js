1ce932e1fdae3ca4e015af619153f803b386f5eb
Camilo Andres Ferrer Bustos <camilo.ferrer@rappi.com>
2019-09-06 23:36:33 -0500

    fix router

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const PersonController = require('./controllers/PersonController');
-const UserController = require('./controllers/UsersController');
 
 console.log(UserController);
 const router = express.Router();
@@ -12,13 +11,5 @@ router.delete('/persons/:id(\\d+)', PersonController.delete);
 router.put('/persons/:id(\\d+)', PersonController.edit);
 router.get('/persons/find_by_name', PersonController.findByName);
 router.get('/persons/all', PersonController.listAll);
-// Users Routes
-router.put('/users/:id', UserController.edit); // edit user
-module.exports = router;
-// Users Routes
-router.post('/users', UserController.save); // Create new users
-router.get('/users/:id(\\d+)', UserController.find); // Find user by id
-router.delete('/users/:id(\\d+)', UserController.delete); // Delete User
-router.get('/users/find_by_name/:name', UserController.findByName);
-router.get('/users/all', UserController.listAll); // List all users
+
 module.exports = router;
