fec0ff5d844a9a4d410fabdfd28f87224183732d
cristian0895 <mangasalucard@hotmail.com>
2019-09-14 09:28:04 -0500

    [resolves #2] change names migration request_student

--- migrations/201909130555_create_Request_student.js
+++ migrations/201909130555_create_Request_student.js
@@ -2,11 +2,11 @@ exports.up = knex =>
   knex.schema.createTable('request_student', (table) => {
     table.integer('id').primary();
     table.string('date');
-    table.string('pship_comp');
-    table.string('n_w_app');
-    table.string('u_agreement');
-    table.string('person_in_ch');
-    table.string('as_met_com');
+    table.string('society');
+    table.string('requester_name');
+    table.string('agreement');
+    table.string('person_in_charge');
+    table.string('how_meet_company');
     table.integer('city_id');
     table.integer('state_id');
     table.integer('practice_type_id');
