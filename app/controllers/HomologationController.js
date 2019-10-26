362deaea7d16973461e5ee4ffd8563438bfa60a4
diegolt8 <diegoalugo@hotmail.com>
2019-10-26 08:56:39 -0500

    [Resolve #11] resolved conflicts

--- app/controllers/HomologationController.js
+++ app/controllers/HomologationController.js
@@ -1,5 +1,6 @@
 const HomologationController = module.exports;
 const HomologationService = require('../services/HomologationService');
+const ErrorHandler = require('../utils/ErrorHandlerMiddleware');
 
 HomologationController.save = async (req, res, next) => {
   const { body } = req;
@@ -9,6 +10,21 @@ HomologationController.save = async (req, res, next) => {
     return res.send();
   } catch (error) {
     console.log({ error });
+
+    return next(error);
+  }
+};
+
+HomologationController.find = async (req, res, next) => {
+  try {
+    const { params: { id } } = req;
+    const homologation = await HomologationService.find(id);
+
+    if (!homologation) return next(new ErrorHandler.BaseError('homologation not exists', 404));
+    
+    return res.send(homologation);
+  } catch (error) {
+    console.log(error);
 
     return next(error);
   }
