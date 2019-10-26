97bed350c8146bb85394144154ea4483502df41e
Gabriela <gabrielaaa1413@gmail.com>
2019-10-26 09:26:53 -0500

    [resolve #13] modificar solicitud

--- app/routes.js
+++ app/routes.js
@@ -27,7 +27,7 @@ router.post('/request', SearchRequestController.save);
 router.get('/homologacion/:id(\\d+)', HomologationController.find);
 router.post('/homologacion', HomologationController.save);
 // Request status
-router.put('/request_company/:id(\\d+)',RequestCompanyController.editStatus);
-router.put('/request_student/:id(\\d+)',RequestStudentController.editStatus);
+router.put('/request_company/:id(\\d+)', RequestCompanyController.editStatus);
+router.put('/request_student/:id(\\d+)', RequestStudentController.editStatus);
 
 module.exports = router;
